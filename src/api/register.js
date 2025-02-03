adc13b5ad30f27c1413d1db75ce084d365aa1eb1
Rimantas <belauzas@gmail.com>
2025-02-03 11:40:12 +0200

    Up

--- src/api/register.js
+++ src/api/register.js
@@ -1,3 +1,6 @@
+import { isValidEmail } from "../../lib/isValidEmail.js";
+import { isValidPassword } from "../../lib/isValidPassword.js";
+
 export function register(obj) {
     if (typeof obj !== 'object'
         || obj === null
@@ -9,6 +12,40 @@ export function register(obj) {
             msg: 'Duomenys turi buti objekte',
         };
     }
+
+    if (Object.keys(obj).length !== 2) {
+        return {
+            status: 'error',
+            msg: 'Netinkama struktura, turi buti email ir password',
+        };
+    }
+
+    const { email, password } = obj;
+
+    if (email === undefined || password === undefined) {
+        return {
+            status: 'error',
+            msg: 'Truksta duomenu, turi buti email ir password',
+        };
+    }
+
+    const emailMsg = isValidEmail(email);
+    if (emailMsg !== '') {
+        return {
+            status: 'error',
+            msg: emailMsg,
+        };
+    }
+
+    if (!isValidPassword(password)) {
+        return {
+            status: 'error',
+            msg: 'Netinkamas slaptazodis',
+        };
+    }
+
+    // jau panaudoto email tikrinimas
+    // irasoma i duombaze
 
     return {
         status: 'success',
--- src/api/register.test.js
+++ src/api/register.test.js
@@ -86,20 +86,115 @@ describe('Netinkamo tipo duomenys', () => {
     });
 });
 
-// describe('Tinkamo tipo, bet neteisingos reiksmes', () => {
-//     test('Demo', () => {
-//         expect(register()).toStrictEqual({
-//             status: 'success',
-//             msg: 'Sekminga registracija',
-//         });
-//     });
-// });
+describe('Tinkamo tipo, bet neteisingos reiksmes', () => {
+    test('Tuscias objektas', () => {
+        expect(register({})).toStrictEqual({
+            status: 'error',
+            msg: 'Truksta duomenu, turi buti email ir password',
+        });
+    });
 
-describe('Teisingos reiksmes', () => {
-    test('Demo', () => {
+    test('Objektas su pertekline informacija', () => {
         expect(register({
             email: 'jonas@jonas.lt',
-            password: '5r4gs51ff65resg5rg445fetr84hd5rgt84',
+            password: 'g48s15fe84gsf5gFGHs4f',
+            extra: true,
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'Netinkama struktura, turi buti email ir password',
+        });
+    });
+
+    test('Truksta email', () => {
+        expect(register({ password: 'e5rg8s45f65as84g' })).toStrictEqual({
+            status: 'error',
+            msg: 'Truksta duomenu, turi buti email ir password',
+        });
+    });
+
+    test('Truksta password', () => {
+        expect(register({ email: 'petras@petras.lt' })).toStrictEqual({
+            status: 'error',
+            msg: 'Truksta duomenu, turi buti email ir password',
+        });
+    });
+
+    test('Netinkamas email', () => {
+        expect(register({
+            email: 2154562,
+            password: 'a5gs4dh51gf26f5sgf'
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'El pastas turi buti teksto tipo.',
+        });
+
+        expect(register({
+            email: '#petras@petras.lt',
+            password: 'a5gs4dh51gf26f5sgf'
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'El pasto dalyje esancioje pries @ simboli, rastas neleistinas "#" simbolis.',
+        });
+
+        expect(register({
+            email: 'petras petraitis@petras.lt',
+            password: 'a5gs4dh51gf26f5sgf'
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'El pasto dalyje esancioje pries @ simboli, rastas neleistinas " " simbolis.',
+        });
+
+        expect(register({
+            email: 'petras@petraitis@petras.lt',
+            password: 'a5gs4dh51gf26f5sgf'
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'El paste turi buti tik 1 @ simbolis.',
+        });
+    });
+
+    test('Netinkamas password', () => {
+        expect(register({
+            email: 'petras@petras.lt',
+            password: []
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'Netinkamas slaptazodis',
+        });
+
+        expect(register({
+            email: 'petras@petras.lt',
+            password: ''
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'Netinkamas slaptazodis',
+        });
+
+        expect(register({
+            email: 'petras@petras.lt',
+            password: 'afsghdyjgfjhgsfasghdjfh'
+        })).toStrictEqual({
+            status: 'error',
+            msg: 'Netinkamas slaptazodis',
+        });
+    });
+});
+
+describe('Teisingos reiksmes', () => {
+    test('jonas', () => {
+        expect(register({
+            email: 'jonas@jonas.lt',
+            password: '5r4gs51ff65resFVGBHJ5fetr84hd5rgt84',
+        })).toStrictEqual({
+            status: 'success',
+            msg: 'Sekminga registracija',
+        });
+    });
+
+    test('maryte', () => {
+        expect(register({
+            email: 'maryte@maryte.lt',
+            password: 'ADFG4514-gshd_+?<',
         })).toStrictEqual({
             status: 'success',
             msg: 'Sekminga registracija',
